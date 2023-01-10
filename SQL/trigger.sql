eb8734303e92e2fab91de35c5ee4582c80d36a37
Shashank Raj Sara <shashanksara03@gmail.com>
2023-01-10 14:08:43 +0530

    updated

--- SQL/trigger.sql
+++ SQL/trigger.sql
@@ -1,8 +1,29 @@
 -- Active: 1671688949427@@127.0.0.1@3308@org
 USE org;
 
-DELIMITER //
 
+----------------------------------------------------------------
+-- CREATE UPDATE TRIGGER
+CREATE TRIGGER after_worker_update 
+    AFTER UPDATE ON worker
+    FOR EACH ROW 
+UPDATE title
+SET 
+    worker_ref_id = NEW.worker_id
+WHERE
+    worker_ref_id = OLD.worker_id;
+
+-- UPDATE
+UPDATE worker 
+SET 
+    worker_id = 100,
+WHERE
+    first_name = 'Niharika';
+
+
+----------------------------------------------------
+
+-- INSERT TRIGGER
 CREATE TRIGGER after_worker_insert
     AFTER INSERT
     ON worker
@@ -12,16 +33,18 @@ BEGIN
             VALUES (
                 NEW.worker_id, "New Joinee", NOW()
             );
-END //
 
-DELIMITER ;
 
+-- INSERT
 INSERT INTO worker
     VALUES (
         10, 'Shashank', 'Raj', 5000, NOW(), 'IT'
     );
 
+----------------------------------------------------------------
+
 SHOW TRIGGERS;
 
-SELECT * from title;
 DROP TRIGGER IF EXISTS after_worker_insert;
+
+
