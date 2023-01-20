9a7cf6e48fab6fa821282a7acaea0424b23b4fb8
Shashank Raj Sara <shashanksara03@gmail.com>
2023-01-20 17:27:42 +0530

    upd

--- SQL/profiler.sql
+++ SQL/profiler.sql
@@ -1,2 +1,14 @@
 
 SELECT @@profiling;
+
+SET profiling=1;
+
+SELECT * FROM hr.regions;
+SELECT * FROM check_extent;
+SELECT * FROM compo_range_hash WHERE sal=3000;
+
+SHOW profiles;
+
+SHOW profile FOR QUERY 14;
+
+SET profiling=0;
