18978deff8d68fffaf7706312ab2a08a96063102
Shashank Raj Sara <shashanksara03@gmail.com>
2023-02-09 15:01:37 +0530

    updated Q6

--- Projects/Healthcare/problems.sql
+++ Projects/Healthcare/problems.sql
@@ -251,7 +251,222 @@ GROUP BY `pharmacyName`
 ORDER BY `Medicine Qty` DESC;
 
 
+
+
+
+
 --Q4
+
+
+/*
+1.“HealthDirect” pharmacy finds it difficult to deal with the product 
+type of medicine being displayed in numerical form, they want the product 
+type in words. Also, they want to filter the medicines based on tax criteria. 
+Display only the medicines of product categories 1, 2, and 3 for medicines 
+that come under tax category I and medicines of product categories 4, 5, and 6 
+for medicines that come under tax category II.
+Write a SQL query to solve this problem.
+ProductType numerical form and ProductType in words are given by
+1 - Generic, 
+2 - Patent, 
+3 - Reference, 
+4 - Similar, 
+5 - New, 
+6 - Specific,
+7 - Biological, 
+8 – Dinamized
+
+3 random rows and the column names of the Medicine table are given for reference.
+Medicine (medicineID, companyName, productName, description, substanceName,
+ productType, taxCriteria, hospitalExclusive, governmentDiscount, taxImunity, maxPrice)
+
+
+12	LIBRA COMERCIO DE PRODUTOS FARMACEUTICOS LTDA	OXALIPLATINA	100 MG PO LIOFILIZADO FR/AMP X 1000 MG	NC/NI	1	I	N	N	N	2373.63
+13	LIBRA COMERCIO DE PRODUTOS FARMACEUTICOS LTDA	SULBACTAM SODICO + AMPICILINA SODICA	1 G + 2 G CT FR AMP VD INC	NC/NI	4	II	N	N	N	29.59
+14	LIBRA COMERCIO DE PRODUTOS FARMACEUTICOS LTDA	PACLITAXEL	6 MG/ML SOL INJ CT FR/AMP X 50 ML	NC/NI	1	I	N	N	N	4122.12
+*/
+
+
+
+
+select medicineID,  productName,
+case `productType`
+when 1 then "Generic"
+when 2 then "Patent"
+when 3 then "Regerence"
+when 4 then "Similar"
+when 5 then "New"
+when 6 then "Specific"
+when 7 then "Biological"
+when 8 then "Dinamized"
+else "-"
+end as `productType`
+from medicine;
+
+
+
+/*
+2.'Ally Scripts' pharmacy company wants to find out the quantity of medicine
+ prescribed in each of its prescriptions.
+Write a query that finds the sum of the quantity of all the
+ medicines in a prescription and if the total quantity of medicine is less 
+ than 20 tag it as “low quantity”. If the quantity of 
+ medicine is from 20 to 49 (both numbers including) tag it as “medium quantity“ 
+ and if the quantity is more than equal to 50 then tag it as “high quantity”.
+Show the prescription Id, the Total Quantity of all the medicines in 
+that prescription, and the Quantity tag for all the prescriptions issued 
+by 'Ally Scripts'.
+3 rows from the resultant table may be as follows:
+prescriptionID	totalQuantity	Tag
+1147561399		43			Medium Quantity
+1222719376		71			High Quantity
+1408276190		48			Medium Quantity
+*/
+
+select * from pharmacy where `pharmacyName`="Ally Scripts";
+with cte as 
+(select DISTINCT(c.prescriptionID) as prescriptionID , sum(c.quantity) totalQuantity
+from contain c
+join prescription pr using (`prescriptionID`)
+join pharmacy ph on pr.`pharmacyID`=ph.`pharmacyID`
+GROUP BY c.`prescriptionID`
+
+)
+
+select c.prescriptionID, c.totalQuantity,
+case 
+    when totalQuantity< 20 then "Low Quantity"
+    when totalQuantity>=20 and totalQuantity <=49 then "Medium Quantity"
+    when totalQuantity>=50 then "High Quantity"
+    else "-"
+    end  
+from cte c 
+join prescription pr on c.`prescriptionID`=pr.`prescriptionID`
+join pharmacy ph on pr.`pharmacyID`=ph.`pharmacyID`
+where `pharmacyName`="Ally Scripts";
+
+
+/*
+3.In the Inventory of a pharmacy 'Spot Rx' the quantity of medicine is considered 
+‘HIGH QUANTITY’ when the quantity exceeds 7500 and ‘LOW QUANTITY’ when the quantity 
+falls short of 1000. The discount is considered “HIGH” if the discount rate on a product
+ is 30% or higher, and the discount is considered “NONE” when the discount rate on a 
+ product is 0%. 'Spot Rx' needs to find all the Low quantity products with high 
+ discounts and all the high-quantity products with no discount so they can adjust 
+ the discount rate according to the demand. 
+Write a query for the pharmacy listing all the necessary details relevant 
+to the given requirement.
+
+Hint: Inventory is reflected in the Keep table.
+*/
+
+select * from keep;
+
+with cte as
+(   select k.medicineid ,k.quantity, 
+    case 
+        when k.quantity>7500 then "HIGH QUANTITY"
+        when k.quantity<1000 then "LOW QUANTITY"
+        else "OK"
+        end as qty_status,
+    case 
+        when k.discount>30 then "HIGH"
+        when k.discount=0 then "NONE"
+        else "NORMAL"
+        end as discount_status
+    from keep k inner join pharmacy ph using(pharmacyid) where ph.`pharmacyName`="Spot Rx")
+select medicineid,quantity,qty_status,discount_status from cte
+where (qty_status="LOW QUANTITY" and discount_status="HIGH") or (qty_status="HIGH QUANTITY" and discount_status="NONE");
+
+
+/*
+4.Mack, From HealthDirect Pharmacy, wants to get a list of all the affordable and costly,
+hospital-exclusive medicines in the database. Where affordable medicines are the medicines 
+that have a maximum price of less than 50% of the avg maximum price of all the medicines in
+the database, and costly medicines are the medicines that have a maximum price of more than 
+double the avg maximum price of all the medicines in the database.  Mack wants clear text
+next to each medicine name to be displayed that identifies the medicine as affordable or 
+costly. The medicines that do not fall under either of the two categories need not be displayed.
+Write a SQL query for Mack for this requirement.
+*/
+
+select productName, `maxPrice` from medicine limit 10;
+
+with cte as
+(select productname as name , `maxPrice` as maxprice, (select round(avg(maxprice),2) from medicine) as avgMed
+from medicine m
+join keep using (`medicineID`)
+join pharmacy p USING(`pharmacyID`)
+where m.hospitalExclusive="S"
+and 
+p.`pharmacyName`="HealthDirect"
+)
+
+
+select name,category from 
+(select name, maxprice, avgMed,
+case
+    when maxprice < 0.5 * avgMed then "Offordable"
+    when maxprice > 2 * avgMed then "Costly"
+    else null
+    end as category
+from cte 
+)a 
+where category is not null;
+
+select round(avg(maxprice),2) from medicine where hospitalExclusive="S";
+
+
+/*
+5. The healthcare department wants to categorize the patients into the following category.
+YoungMale: Born on or after 1st Jan  2005  and gender male.
+YoungFemale: Born on or after 1st Jan  2005  and gender female.
+
+AdultMale: Born before 1st Jan 2005 but on or after 1st Jan 1985 and gender male.
+AdultFemale: Born before 1st Jan 2005 but on or after 1st Jan 1985 and gender female.
+
+MidAgeMale: Born before 1st Jan 1985 but on or after 1st Jan 1970 and gender male.
+MidAgeFemale: Born before 1st Jan 1985 but on or after 1st Jan 1970 and gender female.
+
+ElderMale: Born before 1st Jan 1970, and gender male.
+ElderFemale: Born before 1st Jan 1970, and gender female.
+
+Write a SQL query to list all the patient name, gender, dob, and their category.
+*/
+
+show tables;
+
+CREATE view patient_details  as
+select p.patientid as id ,pe.personname as name ,
+pe.phonenumber as phonenumber, pe.gender as gender,
+pe.addressid as addressid, a.address1 as address, a.city as city, a.state as state,a.zip as zip, p.dob as dob
+from person pe
+join patient p on pe.`personID`=p.`patientID`
+join address a on pe.`addressID`=a.`addressID`;
+
+
+select name,dob,
+case
+
+    when dob >= "2005-01-01" and gender="male" then "Young Male"
+    when dob >= "2005-01-01" and gender="female" then "Young Female"
+
+    when dob < "2005-01-01" and dob >= "1985-01-01" and gender="male" then "Adult Male"
+    when dob < "2005-01-01" and dob >= "1985-01-01" and gender="female" then "Adult Female"
+
+    when dob < "1985-01-01" and dob >= "1970-01-01" and gender="male" then "MidAge Male"  
+    when dob < "1985-01-01" and dob >= "1970-01-01" and gender="female" then "MidAge Female" 
+
+    when dob < "1975-01-01" and gender="male" then "Elder Male"
+    when dob < "1975-01-01" and gender="female" then "Elder Female"
+
+    else "-"
+    end as Category
+
+from patient_details; 
+
+
+
 
 
 
@@ -411,10 +626,10 @@ WITH cte AS (
     GROUP BY state,`diseaseName`
     ORDER BY state,noOftreatments DESC
 )
-SELECT DISTINCT state, FIRST_VALUE(`diseaseName`) OVER(PARTITION BY state) 'MinDisease',
-FIRST_VALUE(noOftreatments) OVER(PARTITION BY state) 'noOfMINtreatments',
-LAST_VALUE(`diseaseName`) OVER(PARTITION BY state) 'MaxDisease',
-LAST_VALUE(noOftreatments) OVER(PARTITION BY state) 'noOfMAXtreatments'
+SELECT DISTINCT state, FIRST_VALUE(`diseaseName`) OVER(PARTITION BY state) 'MaxDisease',
+FIRST_VALUE(noOftreatments) OVER(PARTITION BY state) 'noOfMAXtreatments',
+LAST_VALUE(`diseaseName`) OVER(PARTITION BY state) 'MinDisease',
+LAST_VALUE(noOftreatments) OVER(PARTITION BY state) 'noOfMINtreatments'
 FROM cte;
 
 -- Problem Statement 4: 
@@ -447,9 +662,33 @@ LIMIT 3;
 
 -- Problem Statement 1: 
 -- Insurance companies want to know if a disease is claimed higher or lower than average.  
-Write a stored procedure that returns “claimed higher than average” or “claimed lower than average” when the diseaseID is passed to it. 
+-- Write a stored procedure that returns “claimed higher than average” or “claimed lower than average” 
+-- when the diseaseID is passed to it. 
 -- Hint: Find average number of insurance claims for all the diseases. 
- If the number of claims for the passed disease is higher than the average return “claimed higher than average” otherwise “claimed lower than average”.
+--  If the number of claims for the passed disease is higher than the average return “claimed higher than average” otherwise “claimed lower than average”.
+
+DELIMITER $$
+
+
+CREATE PROCEDURE diseaseClaims(IN custId INT)
+BEGIN
+
+ 
+END $$
+
+DELIMITER;
+
+CALL diseaseClaims();
+
+
+
+
+
+
+
+
+
+
 
 -- Problem Statement 2:  
 -- Joseph from Healthcare department has requested for an application which helps him get genderwise report for any disease. 
